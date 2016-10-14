e4d269485b557b6f71a8556413a25baa40acf1f5
Kxeta <caixeta.phillipe@gmail.com>
2016-10-14 16:52:42 -0300

    Advances made at work

---
+++ client/components/image_list.js
@@ -0,0 +1,25 @@
+//Creats Image List component
+
+// Import React
+
+import React from 'react';
+import ImageDetail from './image_detail';
+
+
+// Create the component
+
+const ImageList = (props) => {
+  const validImages = props.images.filter(image => !image.is_album);
+  const RenderedImages = validImages.map(function(image) {
+    return <ImageDetail key={image .title} image={image}/>
+  });
+
+  return(
+    <ul className="media-list list-group">
+      {RenderedImages}
+    </ul>
+  );
+};
+
+// Export the component
+export default ImageList;
